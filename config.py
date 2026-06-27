9545c1af4ea5ab7bed123dcc015b0f5f9145b73f
ramjirrr123456789-maker <ramjirrr123456789@gmail.com>
2026-06-27 10:31:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheAloneTeam/AloneMusic",
+    "https://github.com/ramjirrr123456789-maker/AloneMusic/tree/master",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
